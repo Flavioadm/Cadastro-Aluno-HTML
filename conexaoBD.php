d9c4202be60b3ff2e0e8a7b5933b20cce7bf1705
Flavioadm <111158006+Flavioadm@users.noreply.github.com>
2023-12-05 00:32:53 -0300

    novo

--- conexaoBD.php
+++ conexaoBD.php
@@ -1,5 +1,15 @@
 <?php
+// Dados de conexão com o banco de dados MySQL
+$hostname = 'db4free.net';
+$username = 'flaviolucas@localhost';
+$password = '3g_@@TaZVbU5Knv';
+$database = 'cadastrodealunos';
 
-$pdo = new PDO('sqlite:CadastroTurma.sqlite');
-
-#$pdo->exec('CREATE TABLE IF NOT EXISTS' site);
+// Estabelecer a conexão com o banco de dados usando o PDO
+try {
+    $pdo = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexão com o banco de dados estabelecida com sucesso.";
+} catch(PDOException $e) {
+    echo "Erro na conexão com o banco de dados: " . $e->getMessage();
+}
